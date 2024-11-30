72fa9c3971055b999b1e8df04605583d4957dc11
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-11-30 20:49:34 +0700

    Add endpoint for PUT

--- upload-r2-assets/src/index.ts
+++ upload-r2-assets/src/index.ts
@@ -1,9 +1,23 @@
 interface Env {
 	MY_BUCKET: R2Bucket;
+	AUTH_SECRET: string;
 }
 
 export default {
 	async fetch(request, env): Promise<Response> {
+		if (request.method === 'PUT') {
+			const auth = request.headers.get('Authorization');
+			const expectedAuth = `Bearer ${env.AUTH_SECRET}`;
+
+			if (!auth || auth != expectedAuth) {
+				return new Response('Unauthorized', { status: 401 });
+			}
+			const url = new URL(request.url);
+			const key = url.pathname.slice(1);
+			await env.MY_BUCKET.put(key, request.body);
+			return new Response('Object ${key} uploaded successfully!');
+		}
+
 		const url = new URL(request.url);
 		const key = url.pathname.slice(1);
 
